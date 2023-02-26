4e0457fd1776b841388b1072700218048bd19b00
abhijitacharya793 <abhijitacharya793@gmail.com>
2023-02-26 12:51:56 +0530

    Added static root

--- katsuro/katsuro/urls.py
+++ katsuro/katsuro/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('home/', include('home.urls')),
-    path('about/', include('about.urls')),
-    path('blog/', include('blog.urls')),
-]
+                  path('admin/', admin.site.urls),
+                  path('home/', include('home.urls')),
+                  path('about/', include('about.urls')),
+                  path('blog/', include('blog.urls')),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
